f95b8761c93a7ff4c8114912c7d907180f5cc11b
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-05-30 06:25:51 +0300

    "Feat: adds url patterns"

---
+++ photos/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns=[
+    re_path('',views.home,name ='images'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^image/<id>',views.image,name='image')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
